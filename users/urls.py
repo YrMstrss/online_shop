49f7a41636a716e0d5cf84c8019577cabc893aac
ValeryYaku <ya.lera55555@gmail.com>
2024-03-04 21:58:36 +0300

    urls for getting JWT

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import RegisterView
@@ -7,4 +8,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
