80c7e96f3fe775e12f51c3e25bfafbfd6dca2e79
ValeryYaku <ya.lera55555@gmail.com>
2024-03-04 20:05:19 +0300

    pagination of products list

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 
 from products.models import Product
+from products.paginators import ProductPaginator
 from products.serializers import ProductSerializer
 
 
 class ProductListAPIView(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    pagination_class = ProductPaginator
