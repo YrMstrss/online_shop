52107e39bd38ef02af518092055281001fadc31c
ValeryYaku <ya.lera55555@gmail.com>
2024-03-04 19:42:19 +0300

    pagination for Categories

--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 
 from categories.models import Category
+from categories.paginators import CategoryPaginator
 from categories.serializers import CategorySerializer
 
 
 class CategoryListAPIView(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    pagination_class = CategoryPaginator
