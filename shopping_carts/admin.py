d96372e86c58f74e9ef7cc292d99e3fca3cf71c8
ValeryYaku <ya.lera55555@gmail.com>
2024-03-04 20:56:30 +0300

    admin pages for user and shopping carts

--- shopping_carts/admin.py
+++ shopping_carts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from shopping_carts.models import ShoppingCart
+
+
+@admin.register(ShoppingCart)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user')
