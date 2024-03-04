0ba73c78824dd964ca8a4439540db074cf33d49b
ValeryYaku <ya.lera55555@gmail.com>
2024-03-04 18:42:25 +0300

    admin page for products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import Category, Subcategory
+from products.models import Product
+
+
+@admin.display(description='Категория')
+def category(obj):
+    parent_category = Subcategory.objects.get(id=obj.subcategory.id).parent_category
+    return Category.objects.get(id=parent_category.id)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price', 'subcategory', category)
