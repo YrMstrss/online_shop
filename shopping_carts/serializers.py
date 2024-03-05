cc331d412b89af1adc2f7e18850641b55488706e
ValeryYaku <ya.lera55555@gmail.com>
2024-03-05 14:52:52 +0300

    output fields for prices

--- shopping_carts/serializers.py
+++ shopping_carts/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from categories.models import Subcategory
-from products.models import Product
 from products.serializers import ProductSerializer
 from shopping_carts.models import ShoppingCart, ProductInCart
 
@@ -9,16 +7,27 @@ from shopping_carts.models import ShoppingCart, ProductInCart
 class ProductInCartSerializer(serializers.ModelSerializer):
 
     product = ProductSerializer()
+    summ = serializers.SerializerMethodField()
 
     class Meta:
         model = ProductInCart
-        fields = ('product', 'count')
+        fields = ('product', 'count', 'summ')
+
+    def get_summ(self, obj):
+        return obj.product.price * obj.count
 
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
 
     products = ProductInCartSerializer(source='productincart_set', many=True)
+    total_sum = serializers.SerializerMethodField()
 
     class Meta:
         model = ShoppingCart
         fields = '__all__'
+
+    def get_total_sum(self, obj):
+        total_summ = 0
+        for product in obj.productincart_set.all():
+            total_summ += product.product.price * product.count
+        return total_summ
