8f9f19b0ddc278304f4c8593674621f70280e89d
ValeryYaku <ya.lera55555@gmail.com>
2024-03-05 17:43:32 +0300

    url and view for adding products in a cart

--- shopping_carts/urls.py
+++ shopping_carts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from shopping_carts.apps import ShoppingCartsConfig
-from shopping_carts.views import CartRetrieveAPIView
+from shopping_carts.views import CartRetrieveAPIView, AddToCartAPIView
 
 app_name = ShoppingCartsConfig.name
 
 urlpatterns = [
     path('', CartRetrieveAPIView.as_view(), name='cart_view'),
+    path('add/<int:product_pk>/', AddToCartAPIView.as_view(), name='add_to_cart')
 ]
--- shopping_carts/views.py
+++ shopping_carts/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from rest_framework import generics
 
+from products.models import Product
 from shopping_carts.models import ShoppingCart, ProductInCart
-from shopping_carts.serializers import ShoppingCartSerializer
+from shopping_carts.serializers import ShoppingCartSerializer, ProductInCartSerializer
 
 
 class CartRetrieveAPIView(generics.RetrieveAPIView):
@@ -10,3 +12,17 @@ class CartRetrieveAPIView(generics.RetrieveAPIView):
 
     def get_object(self):
         return ShoppingCart.objects.get(user=self.request.user)
+
+
+class AddToCartAPIView(generics.GenericAPIView):
+    serializer_class = ProductInCartSerializer
+
+    def get(self, request, *args, **kwargs):
+        product = Product.objects.get(pk=kwargs.get('product_pk'))
+        cart = ShoppingCart.objects.get(user=self.request.user)
+
+        prod = ProductInCart.objects.get_or_create(product=product, cart=cart)
+        prod[0].count += 1
+        prod[0].save()
+
+        return redirect('shopping_carts:cart_view')
