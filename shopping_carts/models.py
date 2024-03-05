34e36e5c4773a0003175769305f0cecebf7236d9
ValeryYaku <ya.lera55555@gmail.com>
2024-03-05 18:00:50 +0300

    error correction

--- shopping_carts/models.py
+++ shopping_carts/models.py
@@ -6,7 +6,7 @@ from users.models import User
 
 class ShoppingCart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
-    total_sum = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='цена всех товаров')
+    total_sum = models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='цена всех товаров')
 
     def __str__(self):
         return f'Корзина {self.user.username}'
@@ -20,7 +20,7 @@ class ProductInCart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='товар')
     count = models.IntegerField(default=0, verbose_name='количество в корзине')
     cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, verbose_name='корзина')
-    summ = models.DecimalField(max_digits=11, decimal_places=2, verbose_name='цена товаров')
+    summ = models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='цена товаров')
 
     def __str__(self):
         return f'{self.product.name} в корзине {self.cart}'
