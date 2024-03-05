48f19456579e2e23aa30571367e79142d5e1aa2d
ValeryYaku <ya.lera55555@gmail.com>
2024-03-05 14:52:40 +0300

    added fields for prices

--- shopping_carts/models.py
+++ shopping_carts/models.py
@@ -6,9 +6,11 @@ from users.models import User
 
 class ShoppingCart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    total_sum = models.IntegerField(verbose_name='цена всех товаров')
 
 
 class ProductInCart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='товар')
     count = models.IntegerField(default=1, verbose_name='количество в корзине')
     cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, verbose_name='корзина')
+    summ = models.IntegerField(verbose_name='цена товаров')
